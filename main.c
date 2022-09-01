5c7744c6239aeb87d1e967565313745ae6243b54
Wolf Mardin <wmardin@c2s15d86.42wolfsburg.de>
2022-09-01 22:34:03 +0200

    first version complete

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 16:46:14 by wmardin           #+#    #+#             */
-/*   Updated: 2022/08/31 11:35:41 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/09/01 22:20:02 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,16 @@ int	my_keypress(int key, t_mlxdata *mlx)
 	if (key == KEY_ESC)
 	{
 		mlx_destroy_window(mlx->mlx, mlx->win);
-		exit(1);
 		return (0);
 	}
 	ft_printf("key:%i\n", key);
 	return (0);
+}
+
+int	my_exit(void)
+{
+	ft_printf("later!\n");
+	exit (1);
 }
 
 int	main(void)
@@ -29,7 +34,10 @@ int	main(void)
 	t_mlxdata	mlxdata;
 	t_imgdata	img;
 	t_circtal	circ;
+	int			x;
+	int			y;
 
+	y = 0;
 	img.width = 1000;
 	img.height = 1000;
 	mlxdata.mlx = mlx_init();
@@ -37,16 +45,31 @@ int	main(void)
 	img.img = mlx_new_image(mlxdata.mlx, img.width, img.height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
-	//my_put_pixel(&img, 5, 5, 0x00FF0000);
-	//my_put_rectangle(&img, 10, 10, 500, 200, 0x00FF0000);
-	my_put_circle(&img, 400, 400, 200, 0x00FFFFFF);
 	circ.iter = 4;
 	circ.r = 600;
 	circ.x = img.width / 2;
 	circ.y = img.height / 2;
 	circ.color = 0x00FFFFFF;
+	//my_put_pixel(&img, 0, 0, 0x00FF0000);
+	//my_put_rectangle(&img, 10, 10, 500, 200, 0x00FF0000);
+	//my_put_circle(&img, 500, 500, 200, 0x00FFFFFF);
 	//my_circtal(&img, circ);
+	while (y < img.height)
+	{
+		x = 0;
+		while (x < img.width)
+		{
+			//my_put_pixel(&img, x, y, 250);
+			if (mandelbrot(x, y, img.height, img.width))
+				my_put_pixel(&img, x, y, 0x00FFFFFF);
+			else
+				my_put_pixel(&img, x, y, 0x00000000);
+			x++;
+		}
+		y++;
+	}
 	mlx_put_image_to_window(mlxdata.mlx, mlxdata.win, img.img, 0, 0);
 	mlx_key_hook(mlxdata.win, my_keypress, &mlxdata);
+	mlx_hook(mlxdata.win, 17, 0L << 0, my_exit, NULL);
 	mlx_loop(mlxdata.mlx);
 }
--- new.c
+++ new.c
@@ -6,27 +6,71 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 16:12:50 by wmardin           #+#    #+#             */
-/*   Updated: 2022/09/01 18:51:58 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/09/01 22:26:27 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "fractol.h"
 
 /*
+Zn1 = Zn0 * Zn0 + c
+*/
+int	mandelbrot(int x, int y, int height, int width)
+{
+	double	r_min;
+	double	r_max;
+	double	i_min;
+	double	i_max;
+	double	cr;
+	double	ci;
+	double	zr;
+	double	zi;
+	int		n;
+	int		n_max;
+	int		set;
+	double	temp;
+
+	set = 1;
+	r_min = -2.0;
+	r_max = 1.0;
+	i_min = -1.5;
+	i_max = i_min + (r_max - r_min) * height / width;
+	cr = r_min + x * (r_max - r_min) / width;
+	ci = i_min + y * (i_max - i_min) / height;
+	n = 0;
+	zr = 0;
+	zi = 0;
+	n_max = 50;
+	while (n < n_max)
+	{
+		if (zr * zr + zi * zi > 4)
+		{
+			set = 0;
+			break ;
+		}
+		temp = 2 * zr * zi + ci;
+		zr = zr * zr - zi * zi + cr;
+		zi = temp;
+		n++;
+	}
+	return (set);
+}
+
+/*
 var a = map(x, 0, width, -2, 2);
 var b = map(y, 0 height, -2, 2);
 */
-int	mandelbrot(int x, int y)
+int	mandelnoob(int x, int y)
 {
 	int		n;
 	int		max;
-	float	a_next;
-	float	b_next;
-	float	a;
-	float	b;
+	int		a_next;
+	int		b_next;
+	int		a;
+	int		b;
 
 	n = 0;
-	max = 50;
+	max = 20;
 	a = x;
 	b = y;
 	while (n < max)
@@ -34,20 +78,13 @@ int	mandelbrot(int x, int y)
 		a_next = a * a - b * b;
 		b_next = 2 * a * b;
 
-		a = a_next + a;
-		b = b_next + b;
+		a = a_next + x;
+		b = b_next + y;
 
-		if ((a + b) > 16)
+		if (abs(a + b) > 2000000000)
 			break ;
 		n++;
 	}
-
-	if (n == max)
-
-	z = z * z + c;
-	a = x * x - y * y
-	b = 2 * x * y + c
-
-	 abs()
-
+	//ft_printf("n:%i\n", n);
+	return (0x00FFFFFF & 0x00000FFF<<n);
 }
